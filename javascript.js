bfb4c87216e063c58beda91c03bb0392f33ffb73
NureddinAkgul <nureddinkgl@gmail.com>
2022-08-19 18:52:26 +0200

    Add clear function
    change e.target.ClassList.add('change-color') on changeColor() function
    to e.target.style.backgroundColor because of hierarchy issues it caused
    on clear function.

--- javascript.js
+++ javascript.js
@@ -16,7 +16,7 @@ const small = 16 * 16;
 const medium = 32 * 32;
 const big = 64 * 64;
 
-sizeClass = {'small': 'small-grid', 'medium': 'medium-grid-default', 'big': 'big-grid'};
+sizeDict = {'small': 'small-grid', 'medium': 'medium-grid-default', 'big': 'big-grid'};
 
 generateGrid();
 
@@ -26,7 +26,7 @@ function changeColor(block) {
     const blocks = document.querySelectorAll('#grid-container > div');
     blocks.forEach(function(block) {
         block.addEventListener('mouseenter', function(e) {
-            isRainbow ? e.target.style.backgroundColor = generateRandomColor() : e.target.classList.add('change-color')})
+            isRainbow ? e.target.style.backgroundColor = generateRandomColor() : e.target.style.backgroundColor = '#413b3b'});
     });
 }
 
@@ -35,10 +35,19 @@ const sizeButtons = document.querySelectorAll('#size > button');
 function changeSize(button) {
     button.addEventListener('click', function(e) {
         let size = e.target.id.toString();
-        generateGrid(eval(size), sizeClass[size]);
+        generateGrid(eval(size), sizeDict[size]);
         changeColor();
     });
 }
+
+const clearButton = document.querySelector('#clear');
+
+function clear() {
+    let grids = document.querySelectorAll('#grid-container > div');
+    grids.forEach(grid => grid.style.backgroundColor = 'white');
+}
+
+clearButton.addEventListener('click', clear);
 
 function generateRandomColor(){
     let maxVal = 0xFFFFFF; // 16777215
