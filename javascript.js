5c388c15850accf1c94ad0758d0c497930395611
NureddinAkgul <nureddinkgl@gmail.com>
2022-08-19 11:44:29 +0200

    add changeSize function

--- javascript.js
+++ javascript.js
@@ -15,15 +15,32 @@ function generateGrid(size = 32 * 44, cssClass = 'medium-grid-default') {
 const small = 16 * 22;
 const medium = 32 * 44;
 const big = 64 * 88;
+
+sizeClass = {'small': 'small-grid', 'medium': 'medium-grid-default', 'big': 'big-grid'};
+
 generateGrid();
 
-let blocks = document.querySelectorAll('#grid-container > div');
+
 
 function changeColor(block) {
-    block.addEventListener("mouseenter", (e) => {
-        // highlight the mouseenter target
-        e.target.classList.add('change-color');
+    const blocks = document.querySelectorAll('#grid-container > div');
+    blocks.forEach(function(block) {
+        block.addEventListener('mouseenter', (e) => {
+            e.target.classList.add('change-color');
+        })
     });
 }
 
-blocks.forEach(changeColor);
+
+const sizeButtons = document.querySelectorAll('#size > button');
+
+function changeSize(button) {
+    button.addEventListener('click', function(e) {
+        let size = e.target.id.toString();
+        generateGrid(eval(size), sizeClass[size]);
+        changeColor();
+    });
+}
+
+sizeButtons.forEach(changeSize);
+changeColor();
